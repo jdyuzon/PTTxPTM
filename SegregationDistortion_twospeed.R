50b99b8437f222ed5c89e705bedae888d02f1ef7
jdyuzon <53333153+jdyuzon@users.noreply.github.com>
2021-04-17 01:16:57 +0200

    Update SegregationDistortion_twospeed.R

--- SegregationDistortion_twospeed.R
+++ SegregationDistortion_twospeed.R
@@ -22,7 +22,7 @@ p.adjust(c(0.03866,0.3284,0.1974,0.6504,1.561e-05,0.000117,0.5224),
 ##############################################################
 ##### Two Speed Genome #######################################
 ##############################################################
-genomicfeatures<-read.table("/Users/jennifer/Desktop/Postdoc_Pteres/QuantGen/SegregationDistortion/genomicfeature.txt")
+genomicfeatures<-read.table("genomicfeature.txt")
 colnames(genomicfeatures)<-c("chrom","start","stop","genomicfeature")
 genomicfeatures$length<-NULL
 genomicfeatures$length[genomicfeatures$chrom=="chr1"]<-5917797
@@ -66,7 +66,7 @@ Pathogenicity_Telomere<-ggboxplot(subset(genomicfeatures, genomicfeature!="repea
 
 
 #### Segregation Distortion and the Two Speed Genome ########
-df<-read.table("/Users/jennifer/Desktop/Postdoc_Pteres/QuantGen/SegregationDistortion/PTT_allgenomicregion_SD_density.txt")
+df<-read.table("PTT_allgenomicregion_SD_density.txt")
 colnames(df)<-c("chrom","start","stop","GeneDensity","NoPathGeneDensity","RepeatDensity","BGCsdensity","effectordensity", "SDdensity")
 df$SD[df$SDdensity>0]<-"significant"
 df$SD[df$SDdensity==0]<-"non-significant"
@@ -120,72 +120,6 @@ SD_twospeed<-ggboxplot(df_genrep, x="SD",y="density",fill="SD",palette =c("blue"
         legend.title = element_blank())
 
 
-########### CDS: dnds pnps compared to segregation distortion
-#MK_SD<-read.table("/Users/jennifer/Desktop/Postdoc_Pteres/Populations/MKtest/MK_results.pttptm.segregdistort.txt")
-MK_SD<-read.table("/Users/jennifer/Desktop/Postdoc_Pteres/Populations/MKtest/MK_results.ptmptt.segregdistort.txt")
-colnames(MK_SD)<-c("chrom","start","stop","CDS",
-                   "ds","dn","ps","pn","dnds","pnps",
-                   "NI","a","DoS",
-                   "SD","allele")
-MK_SD$allele[is.na(MK_SD$allele)]<-"non-significant"
-MK_SD$allele<-factor(MK_SD$allele, levels = c("PTT_sd", "PTM_sd", "non-significant"))
-MK_SD$mk<-MK_SD$dnds/MK_SD$pnps
-
-mean(na.omit(MK_SD$dnds)) #PTT: 0.1973684 #PTM: 0.1973684
-mean(na.omit(MK_SD$pnps)) #PTT: 0.8442814 #PTM: 0.8442814
-mean(na.omit(MK_SD$mk[MK_SD$mk!="Inf"])) #PTT: 0.5104612 #PTM: 0.5104612
-
-median(na.omit(MK_SD$dnds)) #PTT: 0 #PTM: 0
-median(na.omit(MK_SD$pnps)) #PTT: 0.5333333 #PTM: 0.5333333
-median(na.omit(MK_SD$mk[MK_SD$mk!="Inf"])) #PTT: 0 #PTM: 0
-
-sum(na.omit(MK_SD$dn)) #PTT: 115  #PTM: 115
-sum(na.omit(MK_SD$ds)) #PTT: 151  #PTM: 151
-sum(na.omit(MK_SD$pn)) #PTT: 20327 #PTM: 20327
-sum(na.omit(MK_SD$ps)) #PTT: 33408 #PTM: 33408
-
-hist<-list()
-for (stat in c("dnds","pnps","NI","a","DoS")){
-hist[[stat]]<-ggplot(MK_SD, aes_string(x=stat, fill="allele")) +
-  geom_histogram(position="dodge")+
-  theme(legend.position="top")+
-  scale_fill_manual(values=c("red","blue","gray"))+
-  theme(strip.background = element_blank(),
-        strip.placement = "outside",
-        strip.text = element_text(),
-        axis.title=element_text(),
-        axis.ticks.x=element_blank(),
-        legend.position="bottom",
-        legend.key.size = unit(0.5,"cm"),
-        legend.title = element_blank())
-}
-
-boxplot<-list()
-for (stat in c("dnds","pnps","NI","a","DoS","mk")){
-boxplot[[stat]]<-ggboxplot(MK_SD, x="SD",y=stat,fill="SD",palette =c("blue","gray"))+
-  stat_compare_means(size=2,
-                     method="wilcox.test", method.args = list(alternative = "greater"),
-                     comparisons = list(c("significant","non-significant")))+
-  xlab("")+
-  theme(strip.background = element_blank(),
-        strip.placement = "outside",
-        #strip.text = element_text(),
-        axis.title=element_text(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position="bottom",
-        legend.key.size = unit(0.5,"cm"),
-        legend.title = element_blank())
-}
-
-p<-ggarrange(boxplot[["dnds"]],boxplot[["pnps"]],
-          legend="bottom", common.legend = TRUE)
-
-pdf("~/Desktop/Postdoc_Pteres/Populations/MKtest/Figure_SegregDist_dnds_pnps.pdf",
-    width=5, height=5)
-plot(p)
-dev.off()
-
 ## Towards the beginning and ends of chromsomes high repeat/low gene density
 Telomere_GeneDensity<-ggplot(df, aes(x=distance, y=NoPathGeneDensity,)) +  
   geom_point(color="gray", alpha=0.4)+geom_smooth(se = FALSE,colour = "red")+
@@ -212,7 +146,7 @@ SD_twospeed_plots<-ggarrange(Telomere_GeneDensity,
           SD_twospeed,
           widths = c(1.7,2))
 
-pdf("~/Desktop/Postdoc_Pteres/QuantGen/SegregationDistortion/Figure_SegregDist_twospeed.pdf",
+pdf("Figure_SegregDist_twospeed.pdf",
     width=11.9, height=14)
 SD_twospeed_plots
 dev.off()
