7100ebcfe9d786fb6704b02294cf47ce8af1f8b8
jdyuzon <53333153+jdyuzon@users.noreply.github.com>
2022-02-08 17:06:30 -0500

    Update SegregationDistortion.R

--- SegregationDistortion.R
+++ SegregationDistortion.R
@@ -6,6 +6,7 @@ library(plyr)
 library(tidyr)
 library(LDheatmap)
 library(RColorBrewer)
+
 ############################################################################
 #####################Recombination Blocks: Individuals######################
 ############################################################################
@@ -86,7 +87,7 @@ ggplot()+
 ######################Segregation Distortion: Hybrid Population#############
 ############################################################################
 #df<-read.table("~/Downloads/all.recomblocks.bed.tmp", header = TRUE,sep = "\t",na.strings = c(".","0"))
-df<-read.table("/all.recomblocks.bed.tmp", header = TRUE,sep = "\t",na.strings = c(".","0")) #,fill=TRUE
+df<-read.table("all.recomblocks.bed.tmp", header = TRUE,sep = "\t",na.strings = c(".","0")) #,fill=TRUE
 df$chrom <- factor(df$chrom,levels=c(goodChrOrder,
                                      "0-1_contig_32","0-1_contig_39",
                                      "0-1_contig_40","0-1_contig_43",
@@ -338,16 +339,17 @@ genedensity<- data.frame(lapply(genedensity, function(x) {gsub("0-1_contig_m86",
 genedensity[,2:4] <- data.frame(lapply(genedensity[,2:4], function(x) {as.numeric(as.character(x))}))
 genedensity_genome<-genedensity[grepl("chr|mitochondria$", genedensity$chrom), , drop = FALSE]
 genedensity_contig<-genedensity[grepl("0-1_contig", genedensity$chrom), , drop = FALSE]
+genedensity_genome$chrom <- factor(genedensity_genome$chrom,levels=goodChrOrder)
 
 ####format repeats
-repeats<-read.table(PTMxPTT.allrepeat.04292020.bed", header = FALSE,sep = "\t")
-colnames(repeats)<-c("chrom","start","stop","repeat","repeat_type")
-repeats<-repeats[grepl("chrPTT|m86|contig", repeats$chrom), , drop = FALSE]
-repeats<-data.frame(lapply(repeats, function(x) {gsub("PTT_", "", x)}))
-repeats<- data.frame(lapply(repeats, function(x) {gsub("0-1_contig_m86","mitochondria", x)}))
-repeats[,2:3] <- data.frame(lapply(repeats[,2:3], function(x) {as.numeric(as.character(x))}))
-repeats_genome<-repeats[grepl("chr|mitochondria$", repeats$chrom), , drop = FALSE]
-repeats_contig<-repeats[grepl("0-1_contig", repeats$chrom), , drop = FALSE]
+#repeats<-read.table(PTMxPTT.allrepeat.04292020.bed", header = FALSE,sep = "\t")
+#colnames(repeats)<-c("chrom","start","stop","repeat","repeat_type")
+#repeats<-repeats[grepl("chrPTT|m86|contig", repeats$chrom), , drop = FALSE]
+#repeats<-data.frame(lapply(repeats, function(x) {gsub("PTT_", "", x)}))
+#repeats<- data.frame(lapply(repeats, function(x) {gsub("0-1_contig_m86","mitochondria", x)}))
+#repeats[,2:3] <- data.frame(lapply(repeats[,2:3], function(x) {as.numeric(as.character(x))}))
+#repeats_genome<-repeats[grepl("chr|mitochondria$", repeats$chrom), , drop = FALSE]
+#repeats_contig<-repeats[grepl("0-1_contig", repeats$chrom), , drop = FALSE]
 
 ####format effectors
 effectors<-read.table("PTTxPTM_tracks.effectors.bed", header = FALSE,sep = "\t")
@@ -359,7 +361,7 @@ effectors[,2:3] <- data.frame(lapply(effectors[,2:3], function(x) {as.numeric(as
 effectors$type<-"effector"
 effectors_genome<-effectors[grepl("chr|mitochondria$", effectors$chrom), , drop = FALSE]
 effectors_contig<-effectors[grepl("0-1_contig", effectors$chrom), , drop = FALSE]
-
+effectors_genome$chrom <- factor(effectors_genome$chrom,levels=goodChrOrder)
 ####format biosynthetic clusters
 bsc<-read.table("PTTxPTM_tracks.bsc.bed", header = FALSE,sep = "\t")
 colnames(bsc)<-c("chrom","start","stop","region")
@@ -370,7 +372,7 @@ bsc[,2:3] <- data.frame(lapply(bsc[,2:3], function(x) {as.numeric(as.character(x
 bsc$types<-"bsc"
 bsc_genome<-bsc[grepl("chr|mitochondria$", bsc$chrom), , drop = FALSE]
 bsc_contig<-bsc[grepl("0-1_contig", bsc$chrom), , drop = FALSE]
-
+bsc_genome$chrom <- factor(bsc_genome$chrom,levels=goodChrOrder)
 ####format mat loci
 mat<-read.table("matloci.bed", header = FALSE,sep = "\t")
 colnames(mat)<-c("chrom","start","stop")
@@ -378,7 +380,7 @@ mat<-mat[grepl("chrPTT|m86|contig", mat$chrom), , drop = FALSE]
 mat<-data.frame(lapply(mat, function(x) {gsub("PTT_", "", x)}))
 mat[,2:3] <- data.frame(lapply(mat[,2:3], function(x) {as.numeric(as.character(x))}))
 mat$types<-"mat"
-
+mat$chrom <- factor(mat$chrom,levels=goodChrOrder)
 ####format Presence/Absence Variation
 pav<-read.table("PTTvsPTM.nuclearchrmitochondria.shared.txt", header = FALSE,sep = "\t")
 colnames(pav)<-c("chrom","start","stop")
@@ -390,18 +392,18 @@ pav_genome<-pav[grepl("chr|mitochondria$", pav$chrom), , drop = FALSE]
 pav_contig<-pav[grepl("0-1_contig", pav$chrom), , drop = FALSE]
 
 pav_genome$types<-"shared"
-
+pav_genome$chrom <- factor(pav_genome$chrom,levels=goodChrOrder)
 
 ####format recombination breakpoints
-rbreak_genome<-rbreak_dens[grepl("chr|mito", rbreak_dens$chrom), , drop = FALSE]
-rbreak_contig<-rbreak_dens[grepl("0-1_contig", rbreak_dens$chrom), , drop = FALSE]
+#rbreak_genome<-rbreak_dens[grepl("chr|mito", rbreak_dens$chrom), , drop = FALSE]
+#rbreak_contig<-rbreak_dens[grepl("0-1_contig", rbreak_dens$chrom), , drop = FALSE]
 
 ####format regions unique to PTTxPTM segregation distortion
-unique<-read.table("uniq.PTTxPTM.bed", header = FALSE,sep = "\t")
-colnames(unique)<-c("chrom","start","stop")
-unique$type<-"unique"
-unique[,2:3] <- data.frame(lapply(unique[,2:3], function(x) {as.numeric(as.character(x))}))
-unique$chrom = factor(unique$chrom,levels=goodChrOrder)
+#unique<-read.table("uniq.PTTxPTM.bed", header = FALSE,sep = "\t")
+#colnames(unique)<-c("chrom","start","stop")
+#unique$type<-"unique"
+#unique[,2:3] <- data.frame(lapply(unique[,2:3], function(x) {as.numeric(as.character(x))}))
+#unique$chrom = factor(unique$chrom,levels=goodChrOrder)
 #### Format chromosome names
 chrom_names<-c("1","2","3","4","5","6","7","8","9","10","11","12","m")
 names(chrom_names)<-goodChrOrder
@@ -409,8 +411,8 @@ names(chrom_names)<-goodChrOrder
 ### Order of Colors:   
 p<-ggplot() + 
   geom_hline(yintercept = -0.5,color="white",size = 58)+
-  geom_linerange(data=unique,aes(xmin = start, xmax = stop,y=0,
-                                 colour=type,alpha = 0.5),size=300,show.legend = FALSE)+
+  #geom_linerange(data=unique,aes(xmin = start, xmax = stop,y=0,
+  #                               colour=type,alpha = 0.5),size=300,show.legend = FALSE)+
   geom_linerange(data=segreg_genome, aes(xmin = start, xmax = stop, y=freq_ptm,
     colour=significant),size = 5,show.legend = FALSE)+
   facet_grid(~chrom,   scales = "free_x",
@@ -418,15 +420,15 @@ p<-ggplot() +
   scale_color_manual(values=c(
     "bsc"="deepskyblue3",
     "effector"="palegreen3",
-    "hotspot"="red", 
-    "low-simple_repeat"="lightgoldenrodyellow", 
+    #"hotspot"="red", 
+    #"low-simple_repeat"="lightgoldenrodyellow", 
     "mat"="purple",
-    "non-hotspot"="#FFFFFF00",
+    #"non-hotspot"="#FFFFFF00",
     "non-significant"="gray",
     "significant"="blue",
-    "TE"="salmon",
-    "shared"="darkorchid3",
-    "unique"="light gray"
+    #"TE"="salmon",
+    "shared"="darkorchid3"
+    #"unique"="light gray"
    ))+
   geom_hline(yintercept = 0.5)+
   geom_hline(yintercept = -0.08,color="gray")+
@@ -440,16 +442,16 @@ p<-ggplot() +
   scale_fill_gradient(low="#FFFFFF00",high="gray19")+
   #geom_rect(data=repeats_genome,aes(xmin = start, xmax = stop,ymin=-0.25,
   #                                      ymax=-0.2, colour=repeat_type),show.legend = FALSE)+
-  geom_linerange(data=repeats_genome,aes(xmin = start, xmax = stop,y=-0.23,
-                                     colour=repeat_type),size=3.5,show.legend = FALSE)+
+  #geom_linerange(data=repeats_genome,aes(xmin = start, xmax = stop,y=-0.23,
+  #                                   colour=repeat_type),size=3.5,show.legend = FALSE)+
   geom_rect(data=effectors_genome,aes(xmin = start, xmax = stop,ymin=-0.35,
                                     ymax=-0.3, colour=type),show.legend = FALSE)+
   geom_linerange(data=bsc_genome,aes(xmin = start, xmax = stop,y=-0.43,
                                      colour=types),size=3.5,show.legend = FALSE)+
   geom_linerange(data=mat,aes(xmin = start-10000, xmax = stop+10000,y=-0.5,
                               colour=types),size=5,alpha=1,show.legend = FALSE)+
-  geom_linerange(data=rbreak_genome,aes(xmin = start, xmax = stop,y=-0.6,
-                                        colour=significant),size=3.5,show.legend = FALSE)+
+  #geom_linerange(data=rbreak_genome,aes(xmin = start, xmax = stop,y=-0.6,
+  #                                      colour=significant),size=3.5,show.legend = FALSE)+
   geom_linerange(data=pav_genome,aes(xmin = start, xmax = stop,y=-0.7,
                                      colour=types, alpha=0.5),size=3.5,show.legend = FALSE)+
   theme_light()+ theme(panel.margin = unit(0, "lines"),
@@ -476,34 +478,83 @@ for (i in seq_along(strips)) {
   #g$grobs[[strips[i]]]$grobs[[1]]$children[[l]]$children[[1]]$gp$col <- pal[i + 1]
 }
 
-pdf("~/SegregationDistortion_genomicfeatures_nuclmito_PTTxPTM.pdf", width = 21, height=7)
+pdf("SegregationDistortion_genomicfeatures_nuclmito_PTTxPTM.pdf", width = 21, height=7)
 plot(g)
-dev.off()
+graphics.off()
 
-###Contigs
-ggplot() +
-  geom_linerange(data=segreg_contig, aes(xmin = start, xmax = stop, y=freq_ptm,
-                                         colour=significant),size = 5,show.legend = FALSE)+
-  scale_color_manual(values=c("deepskyblue3","palegreen3","red", "salmon","#FFFFFF00","gray","blue", "lightgoldenrodyellow"))+
-  facet_grid(~chrom, scales = "free_x", space = "free_x")+
-  geom_hline(yintercept = 0.5)+
-  theme(panel.spacing = unit(0, "lines"),
-        panel.border = element_rect(color = "light gray", fill = NA, size = 0.5),
-        strip.text.x.top = element_text(angle = 90),
-        axis.text.x = element_text(angle = 90, hjust = 1),
-        axis.ticks.y = element_blank())+  
-  geom_rect(data=genedensity_contig,aes(xmin = start, xmax = stop,ymin=-0.15,
-                                        ymax=-0.1,fill=genecount),show.legend = FALSE)+
-  scale_fill_gradient(low="#FFFFFF00",high="gray19")+
-  geom_linerange(data=repeats_contig,aes(xmin = start, xmax = stop,y=-0.23,
-                                    colour=repeat_type),size=5,show.legend = FALSE)+
-  geom_rect(data=effectors_contig,aes(xmin = start, xmax = stop,ymin=-0.35,
-                                      ymax=-0.3, colour=type),show.legend = FALSE)+
-  geom_linerange(data=bsc_contig,aes(xmin = start, xmax = stop,y=-0.43,
-                                     colour=types),size=5,show.legend = FALSE)+
-  geom_linerange(data=rbreak_contig,aes(xmin = start, xmax = stop,y=-0.53,
-                                        colour=significant),size=5,show.legend = FALSE)+
-  scale_y_continuous(name ="                                  PTM Allele Frequency", 
-                     labels=c("","0","0.5","1.0"))
+#####Candidate Regions
+candidate_regions<-function(CHROM,range_start,range_stop){
+  pal_df<-as.data.frame(rbind(chrom_names,pal))
+  ggplot() + 
+    geom_hline(yintercept = -0.5,color="white",size = 58)+
+    geom_linerange(data=segreg_genome[segreg_genome$chrom==CHROM & 
+                                        segreg_genome$start>range_start-1000000 &
+                                        segreg_genome$stop<range_stop+1000000,], aes(xmin = start, xmax = stop, y=freq_ptm,
+                                                                                     colour=significant),size = 5,show.legend = FALSE)+
+    facet_grid(~chrom,   scales = "free_x",
+               labeller = labeller(chrom = chrom_names))+
+    scale_color_manual(values=c(
+      "bsc"="deepskyblue3",
+      "effector"="palegreen3",
+      "mat"="purple",
+      "non-significant"="gray",
+      "significant"="blue"
+    ))+
+    geom_hline(yintercept = 0.5)+
+    geom_hline(yintercept = -0.08,color="gray")+
+    theme(panel.spacing = unit(0, "lines"),
+          panel.border = element_rect(color = "light gray", fill = NA, size = 0.5),
+          strip.text.x.top = element_text(angle = 90),
+          axis.text.x = element_text(angle = 90, hjust = 1),
+          axis.ticks.y = element_blank())+  
+    geom_rect(data=genedensity_genome[genedensity_genome$chrom==CHROM & 
+                                        genedensity_genome$start>range_start-1000000 &
+                                        genedensity_genome$stop<range_stop+1000000,],aes(xmin = start, xmax = stop,ymin=-0.15,
+                                                                                         ymax=-0.1,fill=genecount),show.legend = FALSE)+
+    scale_fill_gradient(low="#FFFFFF00",high="gray19")+
+    geom_rect(data=effectors_genome[effectors_genome$chrom==CHROM & 
+                                      effectors_genome$start>range_start-1000000 &
+                                      effectors_genome$stop<range_stop+1000000,],aes(xmin = start, xmax = stop,ymin=-0.35,
+                                                                                     ymax=-0.3, colour=type),show.legend = FALSE)+
+    geom_linerange(data=bsc_genome[bsc_genome$chrom==CHROM & 
+                                     bsc_genome$start>range_start-1000000 &
+                                     bsc_genome$stop<range_stop+1000000,],aes(xmin = start, xmax = stop,y=-0.43,
+                                                                              colour=types),size=3.5,show.legend = FALSE)+
+    geom_linerange(data=pav_genome[pav_genome$chrom==CHROM & 
+                                     pav_genome$start>range_start-1000000 &
+                                     pav_genome$stop<range_stop+1000000,],aes(xmin = start, xmax = stop,y=-0.7,
+                                                                              colour=types, alpha=0.5),size=3.5,show.legend = FALSE)+
+    theme_light()+ theme(panel.margin = unit(0, "lines"),
+                         panel.border = element_rect(color = "light gray", fill = NA, size = 0.5),
+                         strip.text.x.top = element_text(size = 20, color = "black",face = "bold"),
+                         strip.background = element_rect(fill=pal_df[2,CHROM]),
+                         axis.ticks.x=element_blank(),
+                         axis.ticks.y=element_blank(),
+                         axis.title=element_text(size=18,face="bold"))+
+    scale_y_continuous(limits=c(-0.75,1),name ="            ", 
+                       labels=c("","","0","0.5","1.0"))+
+    scale_x_continuous(breaks=c(0,1000000,2000000,3000000,4000000,5000000,6000000), 
+                       labels=c("0","1","2","3","4","5","6"))+
+    xlab(paste(CHROM,":", range_start-1000000, "-", range_stop+1000000))
+  
+}
+
+chr3_plot<-candidate_regions(CHROM="chr3",range_start=3617625,range_stop=3618276)+
+  scale_y_continuous(limits=c(-0.75,1),name ="            FGOB10Ptm-1 Allele Frequency", 
+                     labels=c("","","0","0.5","1.0")) ####LD,SD,effectors
+chr6_plot<-candidate_regions(CHROM="chr6",range_start=1503851,range_stop=1504863) ####LD,SD,Dxy,Fst,effectors
+chr10_plot<-candidate_regions(CHROM="chr10",range_start=1206644,range_stop=1264872) ### SD,bsc,(PTT)
+
+candidate_region_plots<-ggarrange(chr3_plot,chr6_plot,chr10_plot, 
+                                  ncol = 3, labels = c("B", "C","D"),
+                                  font.label = list(size = 30, family = "Times"))+
+  theme(plot.margin = margin(1,0,0,0, "cm"))
 
 
+
+
+pdf("Figure3_SegregationDistortion_genomicfeatures_nuclmito_PTTxPTM.pdf", width = 21, height=14)
+print(ggarrange(g, candidate_region_plots, 
+          nrow = 2, labels = "A",
+          font.label = list(size = 30, family = "Times")))
+graphics.off()
